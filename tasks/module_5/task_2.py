842834697296aa11481e255d01abc743e158e323
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-09-14 20:07:32 +0300

    completed module 5

---
+++ tasks/module_5/task_2.py
@@ -0,0 +1,7 @@
+goal = int(input("Число: "))
+
+power = 1
+
+while goal >= 2**(power+1): power += 1
+
+print(power, 2**power)
---
+++ tasks/module_5/task_8.py
@@ -0,0 +1,12 @@
+last = float("inf")
+count = 0
+top = 0
+
+while ( number := int(input("Число: ")) ) != 0:
+    if number == last: count += 1
+    else: 
+        if top < count: top = count 
+        count = 0
+    last = number
+
+print(max(top, count + 1 if count else count))
