a1f87aa235b5f17a61c007ef0e293fc39dd68f86
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-09-20 18:22:16 +0300

    mypy fix

--- tasks/module_7/task_7.py
+++ tasks/module_7/task_7.py
@@ -6,5 +6,5 @@ for _ in range(count):
     for word in input("").split():
         words[word] = words.get(word, 0) + 1
 
-for key in sorted(words, key=words.get, reverse=True):
+for key in sorted(words, key=words.get, reverse=True): # type: ignore
     print(key, words[key])
