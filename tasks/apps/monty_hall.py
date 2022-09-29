746b93d8cc73fecf4c9304699446042e9dfa7ad8
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-09-29 18:54:55 +0300

    simulation variant 2

--- tasks/apps/monty_hall.py
+++ tasks/apps/monty_hall.py
@@ -1,27 +1,32 @@
 import random
 
-COUNT = 1000000
+COUNT = 10000
 
 win_with_change = 0
 win_without_change = 0
 total = 0
 
+def get_opened_door(win_door: int, choice: int) -> int:
+    for i in range(1,4):
+        if i != win_door and i != choice: return i
+
+def switch(choice, doors) -> int:
+    doors.remove(choice)
+    return doors[0]
+
 while total < COUNT:
     total += 1
     doors = [1, 2, 3]
     win_door = random.randint(1, 3)
-    doors.remove(win_door)
     choice = random.randint(1, 3)
-    if choice != win_door:
-        doors.remove(choice)
-    opened_door = random.choice(doors)
-    result_doors = [1, 2, 3]
-    result_doors.remove(opened_door)
-
+    opened_door = get_opened_door(win_door, choice)
+    doors.remove(opened_door)
     if win_door == choice:
         win_without_change += 1
     else:
-        win_with_change += 1
+        choice = switch(choice, doors)
+        if choice == win_door:
+            win_with_change += 1
 
 print(
     f"""Всего дверей открыто:{total}
