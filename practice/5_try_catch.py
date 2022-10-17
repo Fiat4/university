cecd256a3af99ea66fc0718a08c1d3005fc00b5f
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-10-18 00:18:52 +0300

    first try 5

---
+++ practice/5_try_catch.py
@@ -0,0 +1,29 @@
+from cgitb import reset
+
+
+def open_file(filename: str):
+    opened = False
+    try:
+        file = open(filename, "r")
+        opened = True
+        count = int(file.readline())
+
+        return [int(x) for x in [file.readline() for _ in range(count)]]
+
+    except FileNotFoundError:
+        print("Такого файла нет, повторите попытку!")
+
+    except ValueError:
+        print(f"В файле есть не только числа!")
+
+    except Exception as e:
+        print(f"Неопознанная ошибка! {type(e)}, {e=}")
+
+    finally:
+        if opened: file.close()
+
+
+while filename := input("Введите название файла: "):
+    if result := open_file(filename):
+        print(result)
+        break
