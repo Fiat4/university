7700625b05178c2427b54c3a42eafa333a403ab2
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-10-18 22:09:47 +0300

    Practice 6

---
+++ practice/6_re.py
@@ -0,0 +1,14 @@
+import re
+
+SEARCH = re.compile(
+    r"^Рейс (\d+) (?:прибыл|отправился) (из|в) (\S+) в (\d{2}:\d{2}:\d{2}$)"
+)
+
+with open("data/journal.txt", "r", encoding="utf-8") as s:
+    with open("data/cleanjournal.txt", "w", encoding="utf-8") as d:
+        while line := s.readline():
+            results = re.search(SEARCH, line)
+            if results:
+                d.write(
+                    f"[{results.groups()[3]}] Поезд № {results.groups()[0]} {results.groups()[1]} {results.groups()[2]}\n"
+                )
