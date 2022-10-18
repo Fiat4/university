2862667eac5b9b1a53b836df9da00c834556a947
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-10-18 22:39:43 +0300

    practice 7, tasks 1-2

---
+++ practice/7_final.py
@@ -0,0 +1,18 @@
+def get_books(name: str) -> list[tuple[str, str, str, int, float]]:
+    books = []
+    name = name.lower()
+    with open("data/books.csv", 'r', encoding="utf-8") as f:
+        f.readline()
+        for line in f:
+            isbn, title, author, quantity, price = line.strip().split("|")
+            if name in title.lower():
+                books.append((isbn, title, author, int(quantity), float(price)))
+    return books
+
+def get_totals(books: list[tuple[str, str, str, int, float]]) -> list[tuple[str, float]]:
+    response = []
+    for book in books:
+        response.append((book[0], book[3]*book[4]))
+    return response
+
+print(get_totals(get_books("sample")))
