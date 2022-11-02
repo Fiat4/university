9b9d1f00a607ebcf020450ae66520da3d5da6053
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-11-02 12:06:20 +0300

    fixed case

--- practice/3_wheeloffortune/game.py
+++ practice/3_wheeloffortune/game.py
@@ -11,17 +11,17 @@ def game(word: str, lives: int) -> bool:
 
     while lives > 0:
         print(f"{''.join(frame)} | ❤x{lives}")
-        guess = input("Введите букву или слово целиком: ").lower()
+        guess = input("Введите букву или слово целиком: ").upper()
         if guess == "c":
             return True
-        if guess == word:
+        if guess == word.upper():
             print("Вы победили! Приз в студию!")
             return True
         if len(guess) != 1:
             print("Вы ошиблись! Я забираю у вас 1 жизнь")
             lives -= 1
             continue
-        idxs = [index for index in range(size) if guess == word[index]]
+        idxs = [index for index in range(size) if guess.lower() == word[index]]
         if not idxs:
             print("Такой буквы нет! Я забираю у вас 1 жизнь")
             lives -= 1
