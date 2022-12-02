504e6561faae9fa8b277d37b8462f1e12cacac51
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-12-02 13:44:21 +0300

    blacked

--- other/parsing/with_re.py
+++ other/parsing/with_re.py
@@ -4,18 +4,20 @@ import urllib.request
 URL = "https://msk.spravker.ru/avtoservisy-avtotehcentry/?page={}"
 MAX = 6
 
-card_re = re.compile(r'org-widget__in[\w\W]*?widgets-list__item') 
-info_re = re.compile(r'__in">[\W\w]*?title-link">([\W\w]*?)<[\W\w]*?\s*[\W\w]*?location">\s*([А-я,. 0-9]*)[\W\w]*?(?:spec__value">([0-9 + (),-]*)<\/dd>[\W\w]*?)?spec__value">([\W\w]*?)<')
+card_re = re.compile(r"org-widget__in[\w\W]*?widgets-list__item")
+info_re = re.compile(
+    r'__in">[\W\w]*?title-link">([\W\w]*?)<[\W\w]*?\s*[\W\w]*?location">\s*([А-я,. 0-9]*)[\W\w]*?(?:spec__value">([0-9 + (),-]*)<\/dd>[\W\w]*?)?spec__value">([\W\w]*?)<'
+)
 
 
 def parse_page(page: int):
     raw = urllib.request.urlopen(URL.format(page)).read().decode()
     cards = card_re.finditer(raw)
-    for card in cards: 
+    for card in cards:
         info = info_re.search(card.group(0))
         if info:
-            print(info.groups()) 
-    
+            print(info.groups())
+
 
 if __name__ == "__main__":
     for page in range(1, MAX):
