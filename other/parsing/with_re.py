6f4090334f76960d115134d010d880437de602fa
Student 12 <113505765+student12m11ga1k@users.noreply.github.com>
2022-12-02 13:41:51 +0300

    one-line regexp

--- other/parsing/with_re.py
+++ other/parsing/with_re.py
@@ -4,34 +4,17 @@ import urllib.request
 URL = "https://msk.spravker.ru/avtoservisy-avtotehcentry/?page={}"
 MAX = 6
 
-card_re = re.compile(r'org-widget([\w\W]*?)widgets-list__item')
-title_re = re.compile(r'header__title-link">(.+)<\/a>')
-meta_re = re.compile(r'header__meta">\n(.+)<\/span>')
-adress_re = re.compile(r'meta--location">\s+(.+)\s+<\/span>')
-work_time_re = re.compile(r'[А-я-,]+ \d{1,2}:\d{1,2}.\d{1,2}:\d{1,2}')
-phone_re = re.compile(r'(\+7 .\d{3}. \d{3}.\d{2}.\d{2})')
+card_re = re.compile(r'org-widget__in[\w\W]*?widgets-list__item') 
+info_re = re.compile(r'__in">[\W\w]*?title-link">([\W\w]*?)<[\W\w]*?\s*[\W\w]*?location">\s*([А-я,. 0-9]*)[\W\w]*?(?:spec__value">([0-9 + (),-]*)<\/dd>[\W\w]*?)?spec__value">([\W\w]*?)<')
+
 
 def parse_page(page: int):
     raw = urllib.request.urlopen(URL.format(page)).read().decode()
-    cards = re.finditer(card_re, raw)
-    for card in cards:
-        title = re.findall(title_re, card.group(0))
-        meta = re.findall(meta_re, card.group(0))
-        adress = re.findall(adress_re, card.group(0))
-        work_time = re.findall(work_time_re, card.group(0))
-        phone = re.findall(phone_re, card.group(0))
-        
-        print(
-            (
-                title[0] if title else "Неизвестный сервис",
-                meta[0].strip() if meta else "Сервис",
-                adress[0] if adress else "Адрес неизвестен",
-                work_time[0] if work_time else "Часы работы неизвестны",
-                ", ".join(phone) if phone else "Номер неизвестен",            
-            )
-        )
-        
-        
+    cards = card_re.finditer(raw)
+    for card in cards: 
+        info = info_re.search(card.group(0))
+        if info:
+            print(info.groups()) 
     
 
 if __name__ == "__main__":
